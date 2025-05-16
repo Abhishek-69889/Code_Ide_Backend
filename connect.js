6c15337bbe6381a10d5b2a02af63e2f45d714f78
Abhishek-69889 <abhit0396@gmail.con>
2025-05-16 11:33:55 +0530

    Your commit message here

--- connect.js
+++ connect.js
@@ -1,12 +1,16 @@
-// connect.js
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
+  if (mongoose.connection.readyState >= 1) {
+    // Already connected or connecting
+    return;
+  }
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected");
   } catch (err) {
     console.error("MongoDB connection error:", err);
+    process.exit(1);
   }
 };
 
