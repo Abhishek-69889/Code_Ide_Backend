818553b55f16750a961366d93e7fe3aeeed23c47
Abhishek-69889 <abhit0396@gmail.con>
2025-05-17 20:27:14 +0530

    Handle 404 error

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 const express = require('express');
-var createError = require('http-errors');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors');
+const createError = require('http-errors');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
 require('dotenv').config();
+
 const connectDB = require("./connect");
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
 const app = express();
 
 connectDB();
 
-// view engine setup
-
+// view engine setup (if you still want to keep EJS views for some routes)
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -22,17 +22,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-
-
-
-
 
 // CORS setup
-
-
-
 const allowedOrigins = [
   'http://localhost:5173',
   'https://codexa-eta.vercel.app',
@@ -51,7 +42,6 @@ app.use((req, res, next) => {
   next();
 });
 
-// CORS middleware
 app.use(cors({
   origin: function (origin, callback) {
     if (!origin || allowedOrigins.includes(origin)) {
@@ -69,32 +59,32 @@ app.options('*', cors({
   credentials: true
 }));
 
-
-
-
-
-
-
-
+// API routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404
+// Serve React static files from the build folder
+app.use(express.static(path.join(__dirname, 'client/build')));
 
+// SPA fallback: send index.html for any other routes (to let React Router handle them)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
-
 app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
+  // render the error page (EJS)
   res.status(err.status || 500);
   res.render('error');
 });
 
-
-module.exports = app; 
-
+module.exports = app;
